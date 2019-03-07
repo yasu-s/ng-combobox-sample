1c8040ae713e949cbcebd72bf8fe7299021a4cb2
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-03-07 21:14:14 +0900

    数値設定に変更

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,45 @@
 import { Component } from '@angular/core';
 
+interface TestData {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div>
+      <select [(ngModel)]="value" (change)="log()">
+        <option [ngValue]="null">Not Selected</option>
+        <ng-container *ngFor="let item of items">
+          <option [ngValue]="item.id">{{item.name}}</option>
+        </ng-container>
+      </select>
+      <button (click)="init()">Init</button>
+      <button (click)="clear()">Clear</button>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'ng-sample';
+
+  items: TestData[] = [
+    { id: 1, name: 'hige' },
+    { id: 2, name: 'hoge' },
+    { id: 3, name: 'hage' }
+  ]
+
+  value?: number = 2;
+
+  clear(): void {
+    this.value = null;
+    this.log();
+  }
+
+  init(): void {
+    this.value = 2;
+    this.log();
+  }
+
+  log(): void {
+    console.log('value:', this.value);
+  }
 }
